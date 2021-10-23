f9d8174cde295d8293b77a3442c22829ffbb3bf3
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2021-10-24 00:26:42 +0200

    Update PersistenceStrategyStream.java

--- src/org/hbrs/se1/ws21/uebung3/controll/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se1/ws21/uebung3/controll/persistence/PersistenceStrategyStream.java
@@ -9,6 +9,7 @@ import java.io.ObjectOutputStream;
 import java.util.List;
 
 import org.hbrs.se1.ws21.uebung3.controll.persistence.PersistenceException.ExceptionType;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
 
@@ -16,23 +17,26 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
     private String location = "out\\objects.ser";
     private ObjectOutputStream outputFile = null;
     private ObjectInputStream inputFile = null;
-    // Backdoor method used only for testing purposes, if the location should be changed in a Unit-Test
-    // Example: Location is a directory (Streams do not like directories, so try this out ;-)!
+
+    // Backdoor method used only for testing purposes, if the location should be
+    // changed in a Unit-Test
+    // Example: Location is a directory (Streams do not like directories, so try
+    // this out ;-)!
     public void setLocation(String location) {
         this.location = location;
     }
 
     @Override
     /**
-     * Method for opening the connection to a stream (here: Input- and Output-Stream)
-     * In case of having problems while opening the streams, leave the code in methods load
-     * and save
+     * Method for opening the connection to a stream (here: Input- and
+     * Output-Stream) In case of having problems while opening the streams, leave
+     * the code in methods load and save
      */
     public void openConnection() throws PersistenceException {
-        try{
+        try {
             outputFile = new ObjectOutputStream(new FileOutputStream(location));
             inputFile = new ObjectInputStream(new FileInputStream(location));
-        }catch(IOException e){
+        } catch (IOException e) {
             throw new PersistenceException(ExceptionType.ConnectionNotAvailable, "Incorect file location.");
         }
     }
@@ -42,28 +46,39 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
      * Method for closing the connections to a stream
      */
     public void closeConnection() throws PersistenceException {
-
+        try {
+            outputFile.close();
+            inputFile.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     /**
      * Method for saving a list of Member-objects to a disk (HDD)
      */
-    public void save(List<Member> member) throws PersistenceException  {
-
+    public void save(List<Member> member) throws PersistenceException {
+        try {
+            outputFile.writeObject(member);
+            outputFile.flush();
+        } catch (IOException e) {
+            throw new PersistenceException(ExceptionType.ConnectionNotAvailable, "File save error.");
+        }
     }
 
     @Override
     /**
-     * Method for loading a list of Member-objects from a disk (HDD)
-     * Some coding examples come for free :-)
-     * Take also a look at the import statements above ;-!
+     * Method for loading a list of Member-objects from a disk (HDD) Some coding
+     * examples come for free :-) Take also a look at the import statements above
+     * ;-!
      */
-    public List<Member> load() throws PersistenceException  {
+    public List<Member> load() throws PersistenceException {
         // Some Coding hints ;-)
         // ObjectInputStream ois = null;
         // FileInputStream fis = null;
-        // List<...> newListe =  null;
+        // List<...> newListe = null;
         //
         // Initiating the Stream (can also be moved to method openConnection()... ;-)
         // fis = new FileInputStream( " a location to a file" );
@@ -73,10 +88,11 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
         // Object obj = ois.readObject();
 
         // if (obj instanceof List<?>) {
-        //       newListe = (List) obj;
+        // newListe = (List) obj;
         // return newListe
 
         // and finally close the streams (guess where this could be...?)
+        
         return null;
     }
 }
