5fe2319e35bcd1a70ab9decd4d4286669eb0d4e9
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2021-10-27 15:31:27 +0200

    Late but correct

--- src/org/hbrs/se1/ws21/uebung3/controll/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se1/ws21/uebung3/controll/persistence/PersistenceStrategyStream.java
@@ -1,5 +1,6 @@
 package org.hbrs.se1.ws21.uebung3.controll.persistence;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
 import java.io.File;
@@ -18,57 +19,51 @@ import org.hbrs.se1.ws21.uebung3.controll.persistence.PersistenceException.Excep
 public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
 
     // URL of file, in which the objects are stored
-    private String location = "out\\objects.ser";
-    private ObjectOutputStream outputFile = null;
-    private ObjectInputStream inputFile = null;
-    private FileInputStream fis = null;
-    private boolean connection = false;
-    private ByteArrayOutputStream baos = null;
+    private String location = "objects.ser";
+
+    /*
+     * private ObjectOutputStream outputFile = null; private ObjectInputStream
+     * inputFile = null; private FileInputStream fis = null;
+     * 
+     * private ByteArrayOutputStream baos = null;
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
     @Override
     public void openConnection() throws PersistenceException {
-        if (!connection) {
-            try {
-                File localFile = new File(location);
-                if(!localFile.exists()){
-                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(localFile));
-                    oos.writeObject(new ArrayList<>());
-                    oos.close();
-                } 
-                baos = new ByteArrayOutputStream();
-                outputFile = new ObjectOutputStream(baos);
-                fis = new FileInputStream(localFile);
-                inputFile = new ObjectInputStream(fis);
-            } catch (IOException e) {
-                throw new PersistenceException(ExceptionType.ConnectionNotAvailable, "Incorect file location.");
-            }
-            connection = true;
-        }
+        /*
+         * if (!connection) { try { File localFile = new File(location);
+         * if(!localFile.exists()){ ObjectOutputStream oos = new ObjectOutputStream(new
+         * FileOutputStream(localFile)); oos.writeObject(new ArrayList<>());
+         * oos.close(); } baos = new ByteArrayOutputStream(); outputFile = new
+         * ObjectOutputStream(baos); fis = new FileInputStream(localFile); inputFile =
+         * new ObjectInputStream(fis); } catch (IOException e) { throw new
+         * PersistenceException(ExceptionType.ConnectionNotAvailable,
+         * "Incorect file location."); } connection = true; }
+         */
     }
 
     @Override
     public void closeConnection() throws PersistenceException {
-        if (connection) {
-            try {
-                outputFile.close();
-                baos.close();
-                inputFile.close();
-                fis.close();
-            } catch (IOException e) {
-                throw new PersistenceException(ExceptionType.ConnectionNotAvailable, "File save error.");
-            }
-            connection = false;
-        }
+        /*
+         * if (connection) { try { outputFile.close(); baos.close(); inputFile.close();
+         * fis.close(); } catch (IOException e) { throw new
+         * PersistenceException(ExceptionType.ConnectionNotAvailable,
+         * "File save error."); } connection = false; }
+         */
     }
 
     @Override
     public void save(List<Member> member) throws PersistenceException {
         try {
+            FileOutputStream fos = new FileOutputStream(location);
+            ObjectOutputStream outputFile = new ObjectOutputStream(fos);
             outputFile.writeObject(member);
             outputFile.flush();
+            outputFile.close();
+            fos.close();
         } catch (IOException e) {
             throw new PersistenceException(ExceptionType.ConnectionNotAvailable, e.getMessage());
         }
@@ -76,15 +71,17 @@ public class PersistenceStrategyStream implements PersistenceStrategy<Member> {
 
     @Override
     public List<Member> load() throws PersistenceException {
-        if (connection) {
-            try {
-                Object obj = inputFile.readObject();
-                if (obj instanceof List<?>) {
-                    return (List<Member>) obj;
-                }
-            } catch (IOException | ClassNotFoundException cfe) {
-                throw new PersistenceException(ExceptionType.ConnectionNotAvailable, cfe.getMessage());
+        try {
+            FileInputStream fis = new FileInputStream(location);
+            ObjectInputStream inputFile = new ObjectInputStream(fis);
+            Object obj = inputFile.readObject();
+            inputFile.close();
+            fis.close();
+            if (obj instanceof List<?>) {
+                return (List<Member>) obj;
             }
+        } catch (IOException | ClassNotFoundException cfe) {
+            throw new PersistenceException(ExceptionType.ConnectionNotAvailable, cfe.getMessage());
         }
         return new ArrayList<>();
     }
